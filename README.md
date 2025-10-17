a2720d26dbf525be01214b31f6647d8f732591dc
RisingThumb <aaronleonard@risingthumb.xyz>
2025-10-17 02:01:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# func_godot_conditional_model_point_class
+
+This is a helper resource to be used with func_godot. Without func_godot this will not work!
+
+This resource has 4 properties on top of the default FuncGodotFGDPointClass properties:
+
+- Default Model Path (String) (optional): The default path for the model for this path. Leave blank if you do not want a default model.
+- Conditional statements (Dictionary[String, String]) (Optional): A dictionary where the keys are the conditional statements and the values are the model paths.
+- Model Scale Multiplier (Integer): The default scale of the model. If multiply by scale key is ticked, this value will be multiplied by the scale key.
+- Multiply By Scale Key (Bool): Whether to multiply by the scale key that the entity has. This means if you use a scale of 1.2, visually within Trenchbroom, the model will be scaled by 1.2.
+
+All file paths are relative to the game folder you set in Trenchbroom.
+
+When you have set these values as you see fit, you can press the Attach Conditionals to metadata button. This will attach the updated model metadata to the Meta Properties with the model key. This will overwrite anything previously set to the model key in the Meta Properties dictionary.
+
+These conditional models will only show the model conditionally within Trenchbroom. It is still up to you to change the model to the correct mesh or scene whenever your map builds.
+
+As an example:
+![Class image](./conditionalclassimg.png)
+Then in Trenchbroom, I can set model_id key for the model I want, set the scale key and voila:
+![Trenchbroom image](./tbimg.png)
---
+++ func_godot_conditional_model_point_class/func_godot_fgd_conditional_model_point_class.gd
@@ -0,0 +1,47 @@
+@tool
+@icon("res://addons/func_godot/icons/icon_godambler3d.svg")
+class_name FuncGodotFGDConditionalModelPointClass extends FuncGodotFGDPointClass
+## FGD ConditionalModelPointClass entity definition.
+##
+## A resource used to define an FGD ConditionalModelPointClass entity. Extends the regular FuncGodotFGDPointClass.
+## With this you can define a list of conditional models
+##
+## @tutorial(Quake Wiki Entity Article): https://quakewiki.org/wiki/Entity
+## @tutorial(Level Design Book: Entity Types and Settings): https://book.leveldesignbook.com/appendix/resources/formats/fgd#entity-types-and-settings-basic
+## @tutorial(Valve Developer Wiki FGD Article): https://developer.valvesoftware.com/wiki/FGD#Class_Types_and_Properties
+## @tutorial(dumptruck_ds' Quake Mapping Entities Tutorial): https://www.youtube.com/watch?v=gtL9f6_N2WM
+## @tutorial(Level Design Book: Display Models for Entities): https://book.leveldesignbook.com/appendix/resources/formats/fgd#display-models-for-entities
+## @tutorial(Valve Developer Wiki FGD Article: Entity Description Section): https://developer.valvesoftware.com/wiki/FGD#Entity_Description
+## @tutorial(TrenchBroom Manual: Display Models for Entities): https://trenchbroom.github.io/manual/latest/#display-models-for-entities
+
+@export_tool_button("Attach Conditionals to metadata") var metadata_wrangle_action = metadata_wrangle
+@export var defaultModelPath: String = ""
+@export var conditionalStatements: Dictionary[String, String]
+@export var modelScaleMultiplier: int = 32;
+@export var multiplyByScaleKey: bool = true
+
+func metadata_wrangle() -> void:
+	var scaleExpr := '"scale": '+str(modelScaleMultiplier)
+	if multiplyByScaleKey:
+		scaleExpr+="*scale"
+	
+	var modelSwitcher := "";
+	if conditionalStatements or defaultModelPath:
+		modelSwitcher = "{{ "
+	
+	if conditionalStatements:
+		modelSwitcher += ""
+		for conditionalStatement in conditionalStatements.keys():
+			modelSwitcher += conditionalStatement + ' -> { "path": "'+conditionalStatements[conditionalStatement]+'", '+scaleExpr+' }, '
+		if defaultModelPath == "":
+			modelSwitcher = modelSwitcher.trim_suffix(", ")
+	
+	if defaultModelPath != "":
+		modelSwitcher += '{"path": "'+defaultModelPath+'", '+scaleExpr+' }'
+		
+	if conditionalStatements or defaultModelPath:
+		modelSwitcher += " }}"
+	self.meta_properties["model"] = modelSwitcher
+
+func _init() -> void:
+	prefix = "@PointClass"
